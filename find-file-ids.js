021238940648704e89fd2b8873a14cb0d2c290fe
the-overdriven <100090726+the-overdriven@users.noreply.github.com>
2025-05-13 21:07:09 +0200

    hold your horses, do not ban me

--- find-file-ids.js
+++ find-file-ids.js
@@ -7,9 +7,13 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms))
+
 export async function findFileIds(modId) {
   try {
     const url = `https://www.nexusmods.com/morrowind/mods/${modId}?tab=files`
+    await delay(300)
+
     const response = await axios.get(url, {
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
@@ -26,11 +30,8 @@ export async function findFileIds(modId) {
       }
     })
 
-    // console.log('Found data-id values:', dataIds)
     return dataIds
   } catch (error) {
     console.error('Error fetching or parsing page:', error.message)
   }
 }
-
-// findFileIds()
